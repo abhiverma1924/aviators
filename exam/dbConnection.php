9c43df02f002c7a28cf79c29028652f4fe2dbf4a
Neo Anoman <neoanoman@gmail.com>
2019-10-05 01:13:22 +0530

    Update dbConnection.php

--- exam/dbConnection.php
+++ exam/dbConnection.php
@@ -1,6 +1,6 @@
 <?php
 //all the variables defined here are accessible in all the files that include this one
-		$con = mysqli_connect("localhost", "aab96cb9_aviator", "Abhiverma@1819", "aab96cb9_aviation");
+		$con = mysqli_connect("localhost", "root", "", "aab96cb9_aviation");
 		if(!$con){
 			echo "Can't connect database " . mysqli_connect_error($con);
 			exit;
